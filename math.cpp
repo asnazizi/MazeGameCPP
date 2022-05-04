ac2314d4570bfd4dc682001fe74430c7b0686944
sathyagb4 <101268270+sathyagb4@users.noreply.github.com>
2022-05-04 21:30:37 +0530

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,113 @@
+#include <iostream> 
+#include <cstring>
+#include <cstdlib> 
+#include <ctime> 
+#include <fstream>
+#define rounds 3
+
+using namespace std; 
+
+void format(int n, char symbol)
+{
+    
+    for (int i = 0; i < n; i++)
+    {
+        
+        cout << symbol;
+    }
+    
+    cout << endl;
+}
+
+void play_game(int current)
+{
+    int amount = 40; 
+    do
+    {
+    int bet; 
+    int dice; 
+    int guess;
+    srand(time(0));
+    
+    dice = 1 + rand() %10; 
+    
+    cout << "Enter betting amount (must be more than 10) : " ;
+    cin >> bet;
+    cout << endl;
+    
+    if(bet > amount)
+    {
+        cout << "Your betting amount is more than your current balance\n";
+        cout << "Please re-enter amount" << endl; 
+    }
+    
+    cout << "Guess a number between 1 and 10 : "; 
+    cin >> guess;
+    cout << endl; 
+    
+    if(guess <= 0 || guess > 10)
+                cout << "Please check the number!! should be between 1 to 10\n"
+                    <<"\nRe-enter data\n ";
+                    
+    if (dice == guess)
+    {
+        cout << "Congratulation! You won $" << bet*2 << endl; 
+        amount += (bet*2);
+    }
+    
+    else
+    {
+       cout << "Better luck next time! You lost $" << bet << endl; 
+       amount -= bet; 
+    }
+    
+    cout << "The winning number was : " << dice <<endl;
+    cout << endl;
+    
+    cout << "You have $ " << amount << " remaining" << endl;
+    
+    // while(amount <= 10)
+    //     {
+    //         cout << "You have too less money to play";
+    //         break;
+    //     }
+        
+        current++;
+    } while (current != rounds);
+    
+    ofstream fout;    // storing status of game in file.txt
+    fout.open("file.txt");
+    if (amount > 10)
+    {
+        cout << "\nYou won! You may proceed.\n" << endl;    
+        fout << 1 << endl;
+    }
+    else
+    {
+        cout << "\nYou have too less money to continue! Try again.\n" << endl;
+        fout << 0 << endl;
+    }
+
+    fout.close();
+   
+}
+
+int main()
+{
+    format(80, '_');
+    cout << endl;
+    cout << "CASINO GUESSING GAME" << endl;
+    format(80, '_');
+    cout << endl;
+    cout << "1. Choose any number between 1 to 10\n";
+    cout << "2. If you win you will get 2 times of money you bet\n";
+    cout << "3. If you bet on wrong number you will lose your betting amount\n";
+    cout << "To win the game, you must have more than $10 after 3 rounds\n";
+    cout << endl; 
+    cout << "You are provided with an initial deposit of $40" << endl << endl; 
+    format(80, '*');
+    
+    play_game(0);
+    
+}
+
